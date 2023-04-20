fd33929263acb58eed90412eaa625362a2510bbd
syprogrammer <sonaly647@gmail.com>
2023-04-20 18:11:06 +0530

    completed

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("connected to landing page")
+const freetrial = document.getElementById("freetrial");
+freetrial.addEventListener("click", scroll);
+
+
+function scroll() {
+    const element = document.getElementById("loop");
+    element.scrollIntoView();
+  }
